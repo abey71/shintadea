366e285d52fef40274e397acbee76c0107bbd22d
AriSmrd7 <arismrd13@gmail.com>
2021-12-24 21:24:10 +0700

    initial commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$('#login-button').click(function (event) {
+	var userName=document.getElementById("userName").value.toLowerCase();
+		//check
+    if(userName=="rena"){
+			event.preventDefault();
+			$('form').fadeOut(500);
+			$('.wrapper').addClass('form-success');
+			setTimeout(function(){location.href="BirthdayCake.html";},2000);
+	}else{
+		alert("Nama yang dimasukinnya salah ya cantik :)");
+	}
+});
